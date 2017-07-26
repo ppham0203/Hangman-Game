1e4022ebe464117519fdca22ebaa5563a53d473c
Peter Pham <ppham0203@Peters-MacBook-Pro.local>
2017-07-26 16:15:51 -0700

    updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+var randomWord = ["quavo","offset","takeoff","bougie"];
+  // This function is run whenever the user presses a key.
+ document.onkeyup = function(event) {
+
+// Determines which key was pressed.
+ var userGuess = event.key;
+ console.log(userGuess)}
+
+
+var guessRemaining = 0;
+
+function myFunction(){
+
+var chosenWord = randomWord[Math.floor(Math.random() * randomWord.length)];
+console.log(randomWord);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
